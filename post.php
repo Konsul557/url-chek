12b2fe616c4c2d1ed054807d2cc539f2669f6a37
pavel.durandin <pavel.durandin@arealidea.ru>
2020-02-07 14:07:27 +0300

    git commit

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+
+$url = $_POST['url'];
+echo $url;
+
+?>
---
+++ url-check.php
@@ -0,0 +1,30 @@
+<?php
+
+$ch = curl_init();
+$url = readline("Введите  url = ");
+//$url = "http://proweb63.ru/help/php/respons-http-headers";
+$log = fopen('log.txt', "w+");
+
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_NOBODY,true);
+curl_setopt($ch,CURLOPT_HEADER,true);
+
+$head = curl_exec($ch);
+if ($head) {
+    $head_ar = explode(' ', $head);
+    fputs($log ,"HTTP код = ");
+    fputs($log ,$head_ar[1]."\r\n");
+    $time = curl_getinfo($ch);
+    fputs($log ,"Время отклика = ");
+    fputs($log ,$time['total_time']);
+    echo "Проверка успешна. См. файл log.txt"."\r\n" ;
+} else {
+    //echo "Url не удовлетворяет условиям. См. файл log.txt"."\r\n";
+    fputs($log ,"Url не удовлетворяет условиям.");
+}
+fclose($log);
+curl_close($ch);
+
+?>
